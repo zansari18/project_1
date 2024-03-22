f0109b1fdde751542df8fa08bedf6766ce63c424
zansari18 <143058076+zansari18@users.noreply.github.com>
2024-03-21 21:54:26 -0500

    Adding more to my media query

--- site.js
+++ site.js
@@ -2,4 +2,8 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+  $("#box").on("click", function  () {
+    ("#age-verification").prop("checked", true);
+    ("#box").html("<p>&#10004;</p>");
+  });
 });
