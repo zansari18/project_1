e4f18a9e76df467fe6b622322dcd3f5a0a1c1140
zansari18 <143058076+zansari18@users.noreply.github.com>
2024-03-21 22:31:40 -0500

    Trying a new query
    make the text go up

--- site.js
+++ site.js
@@ -2,4 +2,12 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+  $('#email').on('focus', function() {
+    $('#input-email label').addClass('active');
+  });
+  $('#email').on('blur', function() {
+    if($('#email').val().length == 0) {
+      $('#input-email label').removeClass('active');
+    }
+  });
 });
