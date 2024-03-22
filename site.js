76f130da6d6ffb27c07701e1bc606216d651da2c
zansari18 <143058076+zansari18@users.noreply.github.com>
2024-03-21 22:02:24 -0500

    Removed js
    I didn't like how that query looked

--- site.js
+++ site.js
@@ -2,8 +2,4 @@
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
-  $("#box").on("click", function  () {
-    ("#age-verification").prop("checked", true);
-    ("#box").html("<p>&#10004;</p>");
-  });
 });
